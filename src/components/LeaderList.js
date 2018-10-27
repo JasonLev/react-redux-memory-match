4a3bf269d86b2e79bb780c268e93e359fe016639
JasonLev <jasond.levy@gmail.com>
2018-10-26 17:37:40 -0700

    f/seconds single digit

--- src/components/LeaderList.js
+++ src/components/LeaderList.js
@@ -3,10 +3,11 @@ import React from 'react';
 function LeaderList(props) {
   let leaderList = props.highScores.map(
     (score, i) => {
+      let seconds = score.score % 60;
       return (<li key={i} className={i === props.index && "leader-highlight"}>
                 <span className="leader-span">{i + 1}.</span>
                 <span className="leader-name leader-span">{score.name}</span>
-                <span className="leader-score leader-span">{Math.floor(score.score/60)}:{score.score%60}</span>
+                <span className="leader-score leader-span">{Math.floor(score.score/60)}:{seconds < 10 ? "0" + seconds : seconds}</span>
               </li>);
     }
   );
